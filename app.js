8a294804c5bc9d38a8d704306e3a3ac749fbcb31
Damian <dematchuma2017@gmail.com>
2024-05-28 12:31:47 +0300

    dc
    dc

--- app.js
+++ app.js
@@ -20,10 +20,32 @@ function createBoard(){
         const square = document.createElement("div")
         square.classList.add("square")
         square.innerHTML = startPiece
+        square.firstChild && square.firstChild.setAttribute("draggable", "true")
         square.setAttribute("square-id", i)
-        square.classList.add("biege")
+        
+        const row = Math.floor((63 - i) / 8) + 1
+        if(row % 2 === 0){
+            square.classList.add(i % 2 === 0 ? "biege" : "brown")
+        }
+        else{
+            square.classList.add(i % 2 === 0 ? "brown" : "biege")
+        }
+
+        if(i <= 15){
+            square.classList.add("player1-color")
+        }
+        else if(i >= 48){
+            square.classList.add("player2-color")
+        }
+
         gameboard.append(square)
     })
 }
 
 createBoard()
+
+const allSquares = document.querySelectorAll("#gameboard .piece")
+
+allSquares.forEach(square => {
+    square.addEventListener("dragstart", dragStart)
+}
